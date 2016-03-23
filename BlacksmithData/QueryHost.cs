9bf8b97a02ae0053a5dbc1a94c6cc56c57c88cc8
A. Leithner <alexander.leithner@pinguin-systeme.at>
2016-03-23 17:12:25 +0100

    Minor Works on Query Host

--- BlacksmithData/QueryHost.cs
+++ BlacksmithData/QueryHost.cs
@@ -18,11 +18,116 @@ namespace BlacksmithData
             InitializeComponent();
 
             Text = "Blacksmith Query Host - " + path;
+            RunReadXML(path);
         }
 
         private void RunReadXML(string XMLFileToParse)
         {
+            List<string> Keys = null;
+            List<string> Values = null;
 
+            XmlTextReader txtReader = new XmlTextReader(XMLFileToParse);
+            while (txtReader.Read())
+            {
+                XmlNodeType nType = txtReader.NodeType;
+
+                if (nType == XmlNodeType.Element)
+                {
+                    string name = txtReader.Name.ToString();
+                    MessageBox.Show(name);
+                    if (name == "control")
+                    {
+                        Keys = new List<string>();
+                        Values = new List<string>();
+
+                        /*Keys.Add("name");
+                        Values.Add(txtReader.GetAttribute("name"));
+
+                        Keys.Add("type");
+                        Values.Add(txtReader.GetAttribute("type"));
+
+                        Keys.Add("text");
+                        Values.Add(txtReader.GetAttribute("text"));
+
+                        Keys.Add("font");
+                        Values.Add(txtReader.GetAttribute("font"));
+
+                        Keys.Add("weight");
+                        Values.Add(txtReader.GetAttribute("weight"));
+
+                        Keys.Add("fontsize");
+                        Values.Add(txtReader.GetAttribute("fontsize"));
+
+                        Keys.Add("binding");
+                        Values.Add(txtReader.GetAttribute("binding"));
+
+                        Keys.Add("size");
+                        Values.Add(txtReader.GetAttribute("size"));
+
+                        Keys.Add("position");
+                        Values.Add(txtReader.GetAttribute("position"));*/
+                    }
+                }
+                else if (nType == XmlNodeType.Attribute)
+                {
+                    MessageBox.Show(txtReader.Value.ToString());
+
+                    if (Keys != null)
+                    {
+                        Keys.Add(txtReader.Name.ToString());
+                        Values.Add(txtReader.Value.ToString());
+                    }
+                }
+                else if (nType == XmlNodeType.EndElement)
+                {
+                    if (txtReader.Name.ToString() == "control")
+                    {
+                        RunCreateControlViaKeyAndValueList(Keys, Values);
+                        Keys = null;
+                        Values = null;
+                    }
+                }
+            }
+        }
+
+        private void RunCreateControlViaKeyAndValueList(List<string> keys, List<string> values)
+        {
+            string name = values[keys.IndexOf("name")];
+            string type = values[keys.IndexOf("type")];
+            string text = values[keys.IndexOf("text")];
+            string font = values[keys.IndexOf("font")];
+            string weig = values[keys.IndexOf("weight")];
+            string fsiz = values[keys.IndexOf("fontsize")];
+            string bind = values[keys.IndexOf("data-binding")];
+            string size = values[keys.IndexOf("size")];
+            string posi = values[keys.IndexOf("position")];
+            string[] SIZE = size.Split(',');
+            string[] POSI = posi.Split(',');
+
+            Control ctrl = null;
+
+            switch (type)
+            {
+                case "Button":
+                    ctrl = new Button();
+                    break;
+                case "Entry":
+                    ctrl = new TextBox();
+                    break;
+                case "Label":
+                    ctrl = new Label();
+                    break;
+                default:
+                    MessageBox.Show("No recognized Type found in Object \"" + name + "\"!", "Parser Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                    break;
+            }
+
+            ctrl.Text = text;
+            ctrl.Location = new Point(Convert.ToInt16(POSI[0]), Convert.ToInt16(POSI[1]));
+            ctrl.Size = new Size(Convert.ToInt16(SIZE[0]), Convert.ToInt16(SIZE[1]));
+
+            Controls.Add(ctrl);
         }
     }
 }
