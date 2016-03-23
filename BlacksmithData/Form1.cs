2f082f893a83ce6a5c1d0aaec1fa2e16c42e574c
A. Leithner <alexander.leithner@pinguin-systeme.at>
2016-03-23 16:15:07 +0100

    QueryHost addition
    Added QueryHost.cs, which will display the various forms.

--- BlacksmithData/Form1.cs
+++ BlacksmithData/Form1.cs
@@ -54,5 +54,22 @@ namespace BlacksmithData
 
             return Files.ToArray();
         }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Blacksmith Queries", listView1.SelectedItems[0].SubItems[0].Text);
+                QueryHost qh = new QueryHost(path);
+                Hide();
+                qh.FormClosed += new FormClosedEventHandler(QH_Closed);
+                qh.ShowDialog();
+            }
+        }
+
+        void QH_Closed(object sender, FormClosedEventArgs e)
+        {
+            Show();
+        }
     }
 }
